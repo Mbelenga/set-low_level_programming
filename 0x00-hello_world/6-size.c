85de83c36ccb1023cb4c10e029890971f7a44d6d
Mbelenga <mbelengaterence@gmail.com>
2026-02-21 13:41:25 +0300

    Updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Entry point of my program
+*
+* Return: Always 0
+ */
+ int main(void)
+ {
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+ }
