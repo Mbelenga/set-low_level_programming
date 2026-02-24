2aaabb7d9e97feccaff50afa0a78f0ef4effd69e
Mbelenga <mbelengaterence@gmail.com>
2026-02-24 15:48:52 +0300

    fixing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of my program
+ *
+ * Description: Prints alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+    for (letter = 'A'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
