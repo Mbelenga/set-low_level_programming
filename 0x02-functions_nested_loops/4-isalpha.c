724530f6060af48b72dcac7d45401744825145ff
Mbelenga <mbelengaterence@gmail.com>
2026-03-07 12:54:41 +0300

    A function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 /**
  * _isalpha - A function that checks for alphabetic character.
  * @c: Character to be checked
- * 
+ *
  * Return: 1 if c is a letter, otherwise return 0
  */
 int _isalpha(int c)
