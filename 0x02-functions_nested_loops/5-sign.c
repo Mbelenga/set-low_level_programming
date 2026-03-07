1f84b1ce4a6933d6a0d131dc84684bb2a7867f4a
Mbelenga <mbelengaterence@gmail.com>
2026-03-07 13:55:13 +0300

    Updated

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+ * _sign - A function that prints the sign of a number
+ * @n: number to check
+ * 
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero     
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+    _putchar('+');
+    return (1);
+}
+else if (n == 0)
+{
+    _putchar('0');
+    return (0);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}
