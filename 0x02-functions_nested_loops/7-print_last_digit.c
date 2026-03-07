8113b3a330d32d70d2f9a293dd5271ec0a2c73dd
Mbelenga <mbelengaterence@gmail.com>
2026-03-07 14:59:18 +0300

    Updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,7 +5,16 @@
  *
  * Return: value of the last digit
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+		last = -last;
 	
+	_putchar(last + '0');
+
+	return (last);
 }
