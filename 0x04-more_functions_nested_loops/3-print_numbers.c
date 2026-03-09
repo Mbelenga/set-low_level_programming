64d4f2ff423854b5b526370babc7fe9777a386a0
Mbelenga <mbelengaterence@gmail.com>
2026-03-09 12:22:40 +0300

    Fixing an error

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - Function that prints numbers
+ *
+ * Description: prints numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	printf("0123456789");
+	_putchar("\n");
+}
