7ec449c95b2929a7d8a2a9052e07a9e6232bead1
Mbelenga <mbelengaterence@gmail.com>
2026-03-09 21:00:36 +0300

    Done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,4 +9,19 @@ void print_diagonal(int n)
 {
 	int row;
 	int space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (row = 0; row < n; row++)
+	{
+		for (space = 0; space < n; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
