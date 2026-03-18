55929d924c757006b32a66dfabe63e46b5fcfb3f
Mbelenga <mbelengaterence@gmail.com>
2026-03-18 12:14:44 +0300

    Continue

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,10 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
+ * 
+ * Return:Pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n);
