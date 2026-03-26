c8dfe57c05c3eee390925ad3510869255122b163
Mbelenga <mbelengaterence@gmail.com>
2026-03-26 12:03:33 +0300

    A function that locates a character in a string

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,22 +6,22 @@
  */
 int _atoi(char *s)
 {
-        int result = 0;
+		int result = 0;
 
-        int sign = 1;
+		int sign = 1;
 
-        while (*s == '-' || *s == '+')
-        {
-                if (*s == '-')
-                {
-                        sign *= -1;
-                }
-                s++;
-        }
-        while (*s >= '0' && *s <= '9')
-        {
-                result = result * 10 + (*s - '0');
-                s++;
-        }
-        return (result * sign);
+		while (*s == '-' || *s == '+')
+		{
+				if (*s == '-')
+				{
+						sign *= -1;
+				}
+				s++;
+		}
+		while (*s >= '0' && *s <= '9')
+		{
+				result = result * 10 + (*s - '0');
+				s++;
+		}
+		return (result * sign);
 }
